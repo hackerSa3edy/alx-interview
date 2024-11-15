ad84c71e1d18b13e91e7eb9b1fb8ee00ca1c8503
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2024-11-15 02:34:41 +0200

    feat: Add script to fetch and display Star Wars characters
    - Implemented a new script to retrieve and display characters from a specified Star Wars movie using the Star Wars API.
    - Added `getCharacters` function to handle API requests and parse JSON responses.
    - Included `getCharacterBody` function to fetch individual character details asynchronously.
    - Implemented error handling for network issues and invalid responses.
    - Ensured the script adheres to best practices and coding standards.
    
    This update allows users to input a movie ID and retrieve the names of all characters appearing in that movie.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,38 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const args = process.argv;
 
-request(url, (err, res, body) => {
-  if (err) console.log(err);
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (let i = 0; i < characters.length; i++) {
-    const url = characters[i];
-    request(url, (err, res, body) => {
-      if (err) console.log(err);
-      const data = JSON.parse(body);
-      console.log(data.name);
+const getCharacters = (movieId) => {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  request({ url, json: true }, async (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    const charactersUrls = body.characters;
+
+    for (const characterUrl of charactersUrls) {
+      const characterBody = await getCharacterBody(characterUrl);
+      console.log(JSON.parse(characterBody).name);
+    }
+  });
+};
+
+async function getCharacterBody (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      if (response.statusCode === 200) {
+        resolve(body);
+      }
     });
-  }
-});
+  });
+}
+
+const movieId = args[2];
+getCharacters(movieId);
